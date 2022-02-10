24d77e8065a3aca400bcc4d1230f47be91488d1b
Adham Atef <adham.atef45@gmail.com>
2022-02-10 17:39:13 +0200

    comments fixed

--- campaign/models.py
+++ campaign/models.py
@@ -8,7 +8,7 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext as _
 from taggit.managers import TaggableManager
-from user.models import User
+from users.models import User
 
 
 def in_fourteen_days():
--- core/credentials.py
+++ core/credentials.py
@@ -1,4 +1,4 @@
 # Database
-DB_PASSWORD = '0000'
+DB_PASSWORD = 'adham8585'
 DB_HOST = 'localhost'
-DB_USER = 'team4'
+DB_USER = 'postgres'
