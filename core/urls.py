a3c6c14b4b50bc4bdc55193e014c462452080673
Ahmed <magdy691997@gmail.com>
2022-02-12 02:22:59 +0200

    Make The Search Functionality useing a Simulation table in  Database

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('error_404/', include('home.urls')),
     path('auth/', include('authen.urls')),
     path('comment/', include('comment.urls')),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from .models import Test_simulation
 
-def home (req):
-    return render(req , 'home.html')
 
-def error (req):
-    return render(req , '404.html')
+data = Test_simulation.objects.all()
+def home (request):
+    return render(request , 'home.html' , {'name': data})
 
+def error (request):
+    return render(request , '404.html')
+
+def search (request):
+    if request.method == 'POST' and request.POST['search']:
+        search = request.POST['search']
+        projects = Test_simulation.objects.filter(title_simulation__contains = search )
+        if projects :
+            return render(request , 'search_page.html' , {'search' : projects , 'name': data })
+        else:
+            return render(request , 'search_page.html' , {'msg' : 'No Result' , 'name': data })
+    else:
+        return render(request , 'search_page.html' , {'msg' : 'No Result' , 'name': data })
+  
