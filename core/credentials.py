690aa8c70d186688ed3d548ebeb7607c04bbe8ab
ahmedewaidar22 <ahmedewaidar22@gmail.com>
2022-02-12 22:47:59 +0200

    edit

--- core/credentials.py
+++ core/credentials.py
@@ -1,10 +1,10 @@
 # Database
-DB_PASSWORD = '123456'
-DB_HOST = 'localhost'
-DB_USER = 'postgres'
-
-# DB_PASSWORD = '0000'
+# DB_PASSWORD = '123456'
 # DB_HOST = 'localhost'
-# DB_USER = 'team4'
+# DB_USER = 'postgres'
+
+DB_PASSWORD = '0000'
+DB_HOST = 'localhost'
+DB_USER = 'team4'
 
 
