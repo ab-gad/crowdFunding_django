b6d63087ba0fcf2d5b2788df5d56f95b2691a016
Ahmed <magdy691997@gmail.com>
2022-02-13 01:51:52 +0200

    Transform Database From Test_simulation To Campaign Database and Add User in Navbar in auth mood

--- home/views.py
+++ home/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-from .models import Test_simulation
+from unicodedata import category
+from django.shortcuts import render , get_object_or_404
+from campaign.models import Campaign , Category
 from django.shortcuts import redirect
 
-data = Test_simulation.objects.all()
+data = Campaign.objects.all()
 last_5_projects = data.order_by('-id')[:5]
+categories = Category.objects.all()
 
 def home (request):
-    return render(request , 'home.html' , {'name': data , 'last_5_projects':last_5_projects})
+    return render(request , 'home.html' , {'category' : categories , 'last_5_projects':last_5_projects})
 
 def error (request):
     return render(request , '404.html')
@@ -15,12 +17,22 @@ def search (request):
     try:
         if request.method == 'POST' and request.POST['search']:
             search = request.POST['search']
-            projects = Test_simulation.objects.filter(title_simulation__contains = search )
+            projects = Campaign.objects.filter(title__contains = search )
             if projects :
-                return render(request , 'search_page.html' , {'search' : projects , 'name': data })
+                return render(request , 'search_page.html' , {'search' : projects , 'category' : categories , 'name': data })
             else:
-                return render(request , 'search_page.html' , {'msg' : 'No Result' , 'name': data })
+                return render(request , 'search_page.html' , {'msg' : 'No Result' , 'category' : categories , 'name': data })
         else:
-            return render(request , 'search_page.html' , {'msg' : 'No Result' , 'name': data })
+            return render(request , 'search_page.html' , {'msg' : 'No Result' ,'category' : categories , 'name': data })
     except:
         return redirect(error)
+        
+
+def category(request, categoty_id):
+
+    if request.method == "GET":
+        categ = get_object_or_404(Category, id=categoty_id)
+        campaigns = Campaign.objects.filter(category=categoty_id)
+
+    return render(request, 'category.html', {"campaigns": campaigns,"categ": categ, "category": categories})
+# /home/category/2
