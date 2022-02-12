9ded8728297505a983907b77ff83b1223d60b779
ab-gad <philosopher.gad@gmail.com>
2022-02-12 19:03:53 +0200

    user view created

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.profile, name="user_profile"),
+    path('edit/', views.edit, name='user_edit'),
+    path('check_password/', views.check_password, name='check_password')
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from campaign.models import Campaign, Donation
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
+from .forms import EditForm
+from .models import UserProfile
+from django.http import JsonResponse
 
 
 @login_required
@@ -8,3 +11,26 @@ def profile(request):
     current_user_campaigns = Campaign.objects.filter(
         creator_id=request.user.id)
     return render(request, 'profile/base.html', {'campaigns': current_user_campaigns, 'donations': False})
+
+@login_required
+def edit(request):
+    if request.method == "GET":
+        current_user = request.user
+        return render(request, 'user/edit.html', {'current_user': current_user})
+    else:
+        current_user = request.user
+        profile = UserProfile.objects.get(user_id=current_user.id)
+        form = EditForm(request.POST, request.FILES, instance=current_user)
+        if form.is_valid():
+            user = form.save()
+            profile.avatar = user.avatar
+            profile.save()
+            return redirect('user_profile')
+        return render(request, 'user/edit.html', {'current_user': current_user, 'form': form})
+
+@login_required
+def check_password(request):
+    if request.is_ajax and request.method == "POST":
+        password = request.POST.get("pass")
+        is_password_correct = request.user.check_password(password)
+        return JsonResponse({"is_password_correct": is_password_correct})
