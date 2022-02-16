0f01e435a1904b6ea9b97a03166a35092f9e2ed2
Ahmed Nabil <94ahmed.nabil@gmail.com>
2022-02-16 02:57:58 +0200

    password-change

--- authen/urls.py
+++ authen/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path('register/', views.register, name="register"),
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name="logout"),
+    # change password
+    path('password_change/', dj_auth_views.PasswordChangeView.as_view(template_name='authen/password/change.html'),
+         name='password_change'),
 
     
 ]
