761aa14d59ecf722d400432b8b6809a04fdd3005
Adham Atef <adham.atef45@gmail.com>
2022-02-17 17:16:11 +0200

    Adding User to API

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import CampaignList, campaign_detail
+from .views import CampaignList, campaign_detail, user_detail, UserList
 
 router = routers.DefaultRouter()
 router.register(r'Campaign', CampaignList)
+router.register(r'User', UserList)
 urlpatterns = [
     path('', include(router.urls)),
-    path('detail/<int:cam_id>/', campaign_detail),
+    path('cam-detail/<int:cam_id>/', campaign_detail),
+    path('usr-detail/<int:user_id>/', user_detail),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- api/views.py
+++ api/views.py
@@ -7,12 +7,40 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from campaign.models import Campaign
-from .serializers import CampaignSerializer
+from users.models import User
+from .serializers import CampaignSerializer, UserSerializer
 
 
 class CampaignList(viewsets.ModelViewSet):
     queryset = Campaign.objects.all()
     serializer_class = CampaignSerializer
+
+
+class UserList(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, user_id):
+    try:
+        user = User.objects.get(user_id=user_id)
+    except User.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -36,4 +64,3 @@ def campaign_detail(request, cam_id):
     elif request.method == 'DELETE':
         campaign.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
-
