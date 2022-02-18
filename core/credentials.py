d73bea5ff42f6f4525fd26dfece2badb12177a96
ahmedewaidar22 <ahmedewaidar22@gmail.com>
2022-02-19 00:23:52 +0200

    m

--- core/credentials.py
+++ core/credentials.py
@@ -1,11 +1,11 @@
-# Database
-DB_PASSWORD = '123456'
-DB_HOST = 'localhost'
-DB_USER = 'postgres'
-
-# DB_PASSWORD = '0000'
+# # Database
+# DB_PASSWORD = '123456'
 # DB_HOST = 'localhost'
-# DB_USER = 'team4'
+# DB_USER = 'postgres'
+ 
+DB_PASSWORD = '0000'
+DB_HOST = 'localhost'
+DB_USER = 'team4'
 #Social Auth
 SOCIAL_AUTH_FACEBOOK_KEY = '505976934258786'    # App ID
 SOCIAL_AUTH_FACEBOOK_SECRET = '2f0785db6ae0ce3e0cd1d26cff608798'  # App Secret
